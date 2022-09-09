f9c14375c0f22497426aba60f5352711fb72f577
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2022-09-09 11:23:03 -0300

    Adicionando um Middleware

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ const express = require('express');
 const { v4: uuidV4 } = require('uuid');
 
 const app = express();
+app.use(express.json());
 
 const customers = [];
 
-app.use(express.json());
+//Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found' });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
 
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
@@ -28,6 +42,11 @@ app.post('/account', (request, response) => {
   });
 
   return response.status(201).send();
+});
+
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  return response.json(customer.statement);
 });
 
 app.listen(3333);
