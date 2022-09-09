b5870d6b755069317f418e4ffd2b5ffafb70f168
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2022-09-09 10:54:34 -0300

    Att

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 const express = require('express');
+const { v4: uuidV4 } = require('uuid');
 
 const app = express();
 
+const customers = [];
+
 app.use(express.json());
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({
+      error: 'Customer already exists!',
+    });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidV4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
