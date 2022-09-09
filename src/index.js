4d318af4ef1e0c4d4978af01b7f9292483b25a1f
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2022-09-09 15:20:03 -0300

    Criando saque na conta

--- src/index.js
+++ src/index.js
@@ -21,6 +21,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
@@ -41,12 +53,76 @@ app.post('/account', (request, response) => {
     statement: [],
   });
 
-  return response.status(201).send();
+  registeredCustomer = customers.find((customer) => customer.cpf === cpf)
+
+  return response.status(201).send({
+    result: 'success',
+    message: 'Account created successfully',
+    customer: registeredCustomer
+  });
 });
 
 app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request;
-  return response.json(customer.statement);
+
+  const balance = getBalance(customer.statement);
+
+  return response.json({
+    result: 'success',
+    message: 'Statement successfully obtained',
+    statement: customer.statement,
+    balance
+  });
+});
+
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit',
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response
+    .status(201)
+    .send({ result: 'success', message: 'Deposit made successfully' });
+});
+
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({
+      result: 'error',
+      message: 'Insufficient funds',
+      balance: balance,
+    });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  }
+
+  customer.statement.push(statementOperation);
+
+  const updateBalance = getBalance(customer.statement);
+
+  return response.status(201).send({
+    result: 'success',
+    message: 'Successful withdrawal',
+    balance: updateBalance,
+  });
 });
 
 app.listen(3333);
